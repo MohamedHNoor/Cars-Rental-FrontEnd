6b37581ef845d4195e53cbf8e468824d3351de7a
houdamzari <houda.mzari1999@gmail.com>
2023-05-06 01:43:51 +0100

    Update assets

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import 'swiper/css';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import carOne from '../assets/car1.png'
+import carTwo from '../assets/car2.png'
+import carThree from '../assets/car3.png'
+import carFour from '../assets/car4.png'
+import carFive from '../assets/car5.png'
 function Slider(props) {
   return (
     <div>
@@ -10,10 +15,14 @@ function Slider(props) {
       onSlideChange={() => console.log('slide change')}
       onSwiper={(swiper) => console.log(swiper)}
     >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
+      
+      <SwiperSlide>  <img src={carThree} alt="" /> </SwiperSlide>
+      <SwiperSlide>  <img src={carFour} alt="" /> </SwiperSlide>
+      <SwiperSlide>  <img src={carTwo} alt="" /> </SwiperSlide>
+
+      <SwiperSlide>  <img src={carFive} alt="" /> </SwiperSlide>
+      <SwiperSlide>  <img src={carOne} alt="" /> </SwiperSlide>
+
       ...
     </Swiper></div>
   );

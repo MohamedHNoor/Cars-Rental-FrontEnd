6324c0ff7541ace7464fa6925540fb4f3ca4b202
Kashif-Saleem-Ghuman <kashif_saleem_23@hotmail.com>
2023-05-07 23:02:15 +0500

    Chnage style for select button and change background color

--- src/components/Appointments.js
+++ src/components/Appointments.js
@@ -18,15 +18,34 @@ export default function Appointments() {
     setValue(value)
   }
 
+  const customStyles = {
+    container: (provided) => ({
+      ...provided,
+      width: '10vw',
+    }),
+    control: (provided) => ({
+      ...provided,
+      borderRadius: '50px',
+      backgroundColor: '#96cf2c',
+      padding: '0.1vw',
+
+    }),
+    singleValue: (provided) => ({
+      ...provided,
+      color: '#fff',
+    }),
+  };
+
+
   return (
     <>
-      <div className='wrapper background-tint'> 
+      <div className='wrapper background-tint'>
         <div className="container w-50">
           <h1 className='white-text '>BOOK A CAR RIDE</h1>
           <p className='white-text '>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat vitae expedita, eaque nisi laudantium aliquam sapiente excepturi dolorem fugiat ipsum reiciendis ex mollitia reprehenderit ipsa fugit tempore iusto blanditiis ratione accusamus esse cupiditate? Expedita optio corporis quaerat doloribus facere possimus cum veniam nihil officia, impedit quibusdam perferendis laudantium sequi odio?</p>
           <div className="d-flex gap-2 justify-content-center align-items-baseline ">
 
-            <Select className='select-button ' options={options} value={value} onChange={changeHandler} />
+            <Select styles={customStyles} options={options} value={value} onChange={changeHandler} />
             <button type="button" onClick={Home} className="button">Book Now</button>
           </div>
         </div>

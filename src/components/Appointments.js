53c08b29a9a91d501c636f114f4a2c9fa38317bc
Kashif-Saleem-Ghuman <kashif_saleem_23@hotmail.com>
2023-05-08 18:05:21 +0500

    Change styling to make responisve

--- src/components/Appointments.js
+++ src/components/Appointments.js
@@ -9,6 +9,7 @@ export default function Appointments() {
   const navigate = useNavigate();
   const Home = () => {
     navigate("/");
+    console.log("working")
   }
 
   const [value, setValue] = useState('')
@@ -34,6 +35,10 @@ export default function Appointments() {
       ...provided,
       color: '#fff',
     }),
+    placeholder: (provided) => ({
+      ...provided,
+      fontSize: '1vw',
+    }),
   };
 
 
@@ -41,8 +46,9 @@ export default function Appointments() {
     <>
       <div className='wrapper background-tint'>
         <div className="container w-50">
-          <h1 className='white-text '>BOOK A CAR RIDE</h1>
-          <p className='white-text '>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat vitae expedita, eaque nisi laudantium aliquam sapiente excepturi dolorem fugiat ipsum reiciendis ex mollitia reprehenderit ipsa fugit tempore iusto blanditiis ratione accusamus esse cupiditate? Expedita optio corporis quaerat doloribus facere possimus cum veniam nihil officia, impedit quibusdam perferendis laudantium sequi odio?</p>
+          <h1 className='header-book'>BOOK A CAR RIDE</h1>
+          <hr className="horizontal-line" />
+          <p className='description'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat vitae expedita, eaque nisi laudantium aliquam sapiente excepturi dolorem fugiat ipsum reiciendis ex mollitia reprehenderit ipsa fugit tempore iusto blanditiis ratione accusamus esse cupiditate? Expedita optio corporis quaerat doloribus facere possimus cum veniam nihil officia, impedit quibusdam perferendis laudantium sequi odio?</p>
           <div className="d-flex gap-2 justify-content-center align-items-baseline ">
 
             <Select styles={customStyles} options={options} value={value} onChange={changeHandler} />

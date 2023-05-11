a03e5c5f773765c7598f92de4d356216068f871b
Kashif-Saleem-Ghuman <kashif_saleem_23@hotmail.com>
2023-05-11 19:16:06 +0500

    Provide store access to top level component

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from "react-router-dom";
-// import { Provider } from 'react-redux';
-// import store from './redux/store/index';
+import { Provider } from 'react-redux';
+import store from './redux/store/index';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      {/* <Provider store={store}> */}
+      <Provider store={store}>
     <App />
-    {/* </Provider> */}
+    </Provider>
     </Router>
 
   </React.StrictMode>

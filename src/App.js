1b49497330b6153e717f9a48fe489a171bbde7c5
Kashif-Saleem-Ghuman <kashif_saleem_23@hotmail.com>
2023-05-12 12:21:56 +0500

    Update routes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import { Route, Routes } from "react-router-dom";
 import Homepage from "./pages/homepage";
 import Reservationpage from "./pages/reservationpage";
 import Detailspage from "./pages/detailspage";
-import Appointments from "./components/Reservations/Reservations";
 import AddReservations from "./components/Reservations/AddReservations";
 import MyReservations from "./components/Reservations/MyReservations";
 import "./App.css";
 import Reservationshistory from "./pages/reservationshistory";
 import { fetchCars } from "./redux/slices/carSlice";
 import { useDispatch } from "react-redux";
+import Reservations from "./components/Reservations/Reservations";
 
 function App() {
 
@@ -22,8 +22,9 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/bookride" element={<Appointments />} />
         <Route path="/addItem" element={<Reservationpage />} />
+        <Route path="/bookride" element={<Reservations />} />
+
         <Route path="/deleteItem" element={<Reservationshistory />} />
         <Route path="/detailsPage/:id" element={<Detailspage />} />
         <Route path="/reservations/add" element={<AddReservations />} />

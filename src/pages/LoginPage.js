423a9105c2cd55326b87fbe56b39dcad53166000
demix007 <ayodejiabidemi007@gmail.com>
2023-05-13 13:13:04 +0100

    modify LoginPage

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -10,15 +10,3 @@ function LoginPage() {
 }
 
 export default LoginPage;
-import React from 'react';
-import LoginForm from '../components/LoginForm/LoginForm';
-
-function LoginPage() {
-  return (
-    <div>
-      <LoginForm />
-    </div>
-  );
-}
-
-export default LoginPage;

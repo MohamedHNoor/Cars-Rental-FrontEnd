2ba13653cdf5961481709b4272de2bb5f70ae4d6
demix007 <ayodejiabidemi007@gmail.com>
2023-05-13 10:29:01 +0100

    Add functionalities to login page

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import LoginForm from '../components/LoginForm/LoginForm';
+
+function LoginPage() {
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
+}
+
+export default LoginPage;
+import React from 'react';
+import LoginForm from '../components/LoginForm/LoginForm';
+
+function LoginPage() {
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
+}
+
+export default LoginPage;

3c0713dc0728ebfbb4139f5f26e6fbe20ac561d2
Kashif-Saleem-Ghuman <kashif_saleem_23@hotmail.com>
2023-05-14 15:03:14 +0500

    Refactor date picker to ensure that return_date is not less than the pick_up date, pickup is not less than the current date, return date is not empty

--- src/components/Date Picker/DatePicker.js
+++ src/components/Date Picker/DatePicker.js
@@ -1,7 +1,6 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-
 
 const DateRange = ({
   selectedStartDate,
@@ -9,14 +8,44 @@ const DateRange = ({
   onStartDateChange,
   onEndDateChange,
 }) => {
-  
-  const [startDate, setStartDate] = useState(new Date());
-  const [endDate, setEndDate] = useState(new Date());
-  const onChange = (dates) => {
-    const [start, end] = dates;
-    setStartDate(start);
-    setEndDate(end);
+  const [startDate, setStartDate] = useState(selectedStartDate);
+  const [endDate, setEndDate] = useState(selectedEndDate);
+
+  useEffect(() => {
+    if (!selectedStartDate) {
+      setEndDate(null);
+      onEndDateChange(null);
+    }
+  }, [selectedStartDate, onEndDateChange]);
+
+  const handleStartDateChange = (date) => {
+    setStartDate(date);
+    onStartDateChange(date);
+    if (!selectedEndDate || date > selectedEndDate) {
+      setEndDate(null);
+      onEndDateChange(null);
+    }
   };
+
+  const handleEndDateChange = (date) => {
+    setEndDate(date);
+    onEndDateChange(date);
+  };
+
+  useEffect(() => {
+    if (selectedStartDate && !selectedEndDate) {
+      setEndDate(null);
+      onEndDateChange(null);
+    }
+  }, [selectedStartDate, selectedEndDate, onEndDateChange]);
+
+  useEffect(() => {
+    if (selectedStartDate && selectedEndDate && selectedEndDate < selectedStartDate) {
+      setEndDate(null);
+      onEndDateChange(null);
+    }
+  }, [selectedStartDate, selectedEndDate, onEndDateChange]);
+
   return (
     <>
       <label className="mb-2 text-white h5" htmlFor="start-date">
@@ -25,11 +54,12 @@ const DateRange = ({
       <DatePicker
         className="mb-4 add-input"
         selected={selectedStartDate}
-        onChange={date => onStartDateChange(date)}
+        onChange={handleStartDateChange}
         selectsStart
-        startDate={selectedStartDate}
+        startDate={startDate}
         endDate={selectedEndDate}
         placeholderText="Select start date"
+        minDate={new Date()} // this is to disable past dates
       />
       <label className="mb-2 text-white h5" htmlFor="start-date">
         End Date:
@@ -37,11 +67,13 @@ const DateRange = ({
       <DatePicker
         className="mb-4 add-input"
         selected={selectedEndDate}
-        onChange={date => onEndDateChange(date)}
+        onChange={handleEndDateChange}
         selectsEnd
-        startDate={selectedStartDate}
+        startDate={startDate}
         endDate={selectedEndDate}
         minDate={selectedStartDate}
+        placeholderText="Select end date"
+        disabled={!selectedStartDate} // Disable the end date if no start date is selected
       />
     </>
   );

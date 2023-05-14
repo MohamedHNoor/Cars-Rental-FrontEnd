780fd73f5eef2af8547234f6b43087a327be4354
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-05-15 00:00:18 +0200

    Add toaster for notifications popup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
-// import frtchCars from './components/';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Homepage from './pages/homepage';
 import LifeStyle from './pages/lifestyle';
 import CarHistory from './components/CarHistory/CarHistory';
@@ -44,6 +45,7 @@ function App() {
           <Route path="/register" element={<LoginPage />} />
           <Route path="/*" element={<Error />} />
         </Routes>
+        <ToastContainer position="top-center" />
       </BrowserRouter>
     </div>
   );

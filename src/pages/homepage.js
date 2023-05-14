249389918341b3fcdc70810bf4ddbdd255441cb2
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-05-15 00:05:34 +0200

    Add sidebar to the pages

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import Slider from "../components/Slider/Slider";
-import Swiper from "swiper";
-import '../components/Slider/Slider.css'
-import SideBar from "../components/SideBar/Sidebar";
+import Slider from '../components/Slider/Slider';
+import Swiper from 'swiper';
+import '../components/Slider/Slider.css';
+import SideBar from '../components/SideBar/Sidebar';
+import SmallSidebar from '../components/SmallSidebar/SmallSidebar';
+import BigSidebar from '../components/BigSidebar/BigSidebar';
+
 function Homepage(props) {
   return (
     <div className="sliderwrapper">
-      <SideBar />
+      <SmallSidebar />
+      <BigSidebar />
       <div className="sliderWrapper">
         <Slider />
       </div>
-      
     </div>
   );
 }

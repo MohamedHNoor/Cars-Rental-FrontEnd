8952116ddde001073a6539da673e8697358dd16f
Kashif-Saleem-Ghuman <kashif_saleem_23@hotmail.com>
2023-05-14 14:01:58 +0500

    Add date range selector fields

--- src/components/Reservations/Reservations.js
+++ src/components/Reservations/Reservations.js
@@ -3,11 +3,14 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import SideBar from "../SideBar/Sidebar";
 import { createReservation } from "../../redux/slices/carSlice";
-
+import DateRange from "../Date Picker/DatePicker";
+import "react-datepicker/dist/react-datepicker.css";
 function Reservations(props) {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const { cars } = useSelector((state) => state.cars);
+  const [selectedStartDate, setSelectedStartDate] = useState(new Date());
+  const [selectedEndDate, setSelectedEndDate] = useState(new Date());
   console.log("Cars fetched: ", cars);
   const pickUpDate = new Date(2023, 5, 13); // Month is 0-based (0: January, 1: February, etc.)
   const returnDate = new Date(2023, 5, 29); // Month is 0-based (0: January, 1: February, etc.)
@@ -18,25 +21,24 @@ function Reservations(props) {
     car_id: 0, // initially set to 0
   });
 
-
   const handleBookNow = () => {
     // Find the selected car based on its id
-    const selectedCar = cars.find((car) => car.id === parseInt(reservationData.car_id));
-    console.log("selected car: ",selectedCar)
-    console.log("reservationData: ",reservationData.car_id)
+    const selectedCar = cars.find(
+      (car) => car.id === parseInt(reservationData.car_id)
+    );
+    console.log("selected car: ", selectedCar);
+    console.log("reservationData: ", reservationData.car_id);
     if (selectedCar) {
       // Update the car_id with the actual car_id value
       const carId = selectedCar.id;
-      console.log("carID: ",carId) // Ensure the car_id is a string
-      const updatedReservationData = { ...reservationData, car_id: carId };
+      console.log("carID: ", carId);
+      const updatedReservationData = { ...reservationData, car_id: carId, pick_up: selectedStartDate.toISOString(),
+        return_date: selectedEndDate.toISOString(), };
       dispatch(createReservation(updatedReservationData));
-      navigate("/reservations/add");
+      navigate("/myreservations");
     }
   };
-  
-  
-  
-  
+
   return (
     <>
       <div className="sliderwrapper">
@@ -55,7 +57,7 @@ function Reservations(props) {
               veniam nihil officia, impedit quibusdam perferendis laudantium
               sequi odio?
             </p>
-            <div className="d-flex gap-2 justify-content-center align-items-baseline ">
+            <div className="d-flex flex-column gap-2 justify-content-center align-items-baseline ">
               <select
                 className="select-car"
                 value={reservationData.car_id}
@@ -70,10 +72,25 @@ function Reservations(props) {
                   Select car
                 </option>
                 {cars.map((car) => (
-                  <option key={car.id}  value={car.id}>
+                  <option key={car.id} value={car.id}>
                     {car.name}
                   </option>
                 ))}
+              </select>
+              <DateRange
+              selectedStartDate={selectedStartDate}
+              selectedEndDate={selectedEndDate}
+              onStartDateChange={setSelectedStartDate}
+              onEndDateChange={setSelectedEndDate}
+              />
+              <select className="mb-4 add-input">
+                <option disabled selected value="">
+                  Select City
+                </option>
+                <option value="London">London</option>
+                <option value="Manchester">Manchester</option>
+                <option value="Glasgow">Glasgow</option>
+                <option value="Bristol">Bristol</option>
               </select>
 
               <button type="button" onClick={handleBookNow} className="button">

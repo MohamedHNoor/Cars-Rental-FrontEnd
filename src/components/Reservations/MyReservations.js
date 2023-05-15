8b120bb5cea4dc45ec47e4480327853645c7652c
Kashif-Saleem-Ghuman <kashif_saleem_23@hotmail.com>
2023-05-15 16:31:47 +0500

    Refactored to style for mobile version

--- src/components/Reservations/MyReservations.js
+++ src/components/Reservations/MyReservations.js
@@ -23,25 +23,35 @@ const MyReservations = () => {
         <BigSidebar />
         <SmallSidebar />
         <div className="wrapper-my-reservation background-tint overflow-auto">
-          <h2 className="text-white text-center my-3 h1  bg-dark p-2">
+          <h2 className="text-white text-center my-3 h1 bg-dark p-2 mt-5">
             My Reservations
           </h2>
-          <div className="d-flex flex-wrap gap-5 my-4 ms-4 overflow-auto">
+          <div className="row justify-content-center p-3">
             {reservations.map((reservation) => (
               <div
-                className="d-flex flex-column flex-wrap border text-white rounded border-white p-3 bg-dark bg-opacity-50"
+                className="col-10 col-md-6 col-lg-4 col-xl-3 my-4"
                 key={reservation.id}
               >
-                <h2 className="text-decoration-underline">{reservation.car}</h2>
-                <p>City: {reservation.city}</p>
-                <p>Pick-up Date: {reservation.pick_up}</p>
-                <p>Return Date: {reservation.return_date}</p>
-                <button
-                  className="btn btn-danger"
-                  onClick={() => handleDelete(reservation.id)}
-                >
-                  Delete
-                </button>
+                <div className="card text-white bg-dark bg-opacity-75  border ">
+                  <div className="card-body">
+                    <h2 className="card-title text-decoration-underline">
+                      {reservation.car}
+                    </h2>
+                    <p className="card-text">City: {reservation.city}</p>
+                    <p className="card-text">
+                      Pick-up Date: {reservation.pick_up}
+                    </p>
+                    <p className="card-text">
+                      Return Date: {reservation.return_date}
+                    </p>
+                    <button
+                      className="btn btn-danger"
+                      onClick={() => handleDelete(reservation.id)}
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </div>
               </div>
             ))}
           </div>

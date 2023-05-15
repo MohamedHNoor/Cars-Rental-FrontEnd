c368173b262f61fddb634ca620c86541ac290f7d
Kashif-Saleem-Ghuman <kashif_saleem_23@hotmail.com>
2023-05-15 16:12:12 +0500

    Restyle page

--- src/components/Reservations/MyReservations.js
+++ src/components/Reservations/MyReservations.js
@@ -22,22 +22,27 @@ const MyReservations = () => {
       <div className="sliderwrapper">
         <BigSidebar />
         <SmallSidebar />
-        <div className="wrapper-my-reservation background-tint">
-          <h2>MyReservations</h2>
-          {reservations.map((reservation) => (
-            <div key={reservation.id}>
-              <h2>{reservation.car}</h2>
-              <p>City: {reservation.city}</p>
-              <p>Pick-up Date: {reservation.pick_up}</p>
-              <p>Return Date: {reservation.return_date}</p>
-              <button
-                className="btn btn-danger"
-                onClick={() => handleDelete(reservation.id)}
+        <div className="wrapper-my-reservation background-tint overflow-auto">
+          <h2 className=" text-center my-3 h1  bg-light bg-opacity-25 p-2">MyReservations</h2>
+          <div className="d-flex flex-wrap gap-5 my-4 ms-4 overflow-auto">
+            {reservations.map((reservation) => (
+              <div
+                className="d-flex flex-column flex-wrap border text-white rounded border-white p-3 bg-dark bg-opacity-25"
+                key={reservation.id}
               >
-                Delete
-              </button>
-            </div>
-          ))}
+                <h2>{reservation.car}</h2>
+                <p>City: {reservation.city}</p>
+                <p>Pick-up Date: {reservation.pick_up}</p>
+                <p>Return Date: {reservation.return_date}</p>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => handleDelete(reservation.id)}
+                >
+                  Delete
+                </button>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </>

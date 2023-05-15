efd15cfad116b7614acea9b92417090c36f9d3d7
Kashif-Saleem-Ghuman <kashif_saleem_23@hotmail.com>
2023-05-15 16:15:43 +0500

    Refactor to change add undlerline for car and change style for heading

--- src/components/Reservations/MyReservations.js
+++ src/components/Reservations/MyReservations.js
@@ -23,14 +23,16 @@ const MyReservations = () => {
         <BigSidebar />
         <SmallSidebar />
         <div className="wrapper-my-reservation background-tint overflow-auto">
-          <h2 className=" text-center my-3 h1  bg-light bg-opacity-25 p-2">MyReservations</h2>
+          <h2 className="text-white text-center my-3 h1  bg-dark bg-opacity-75 p-2">
+            My Reservations
+          </h2>
           <div className="d-flex flex-wrap gap-5 my-4 ms-4 overflow-auto">
             {reservations.map((reservation) => (
               <div
                 className="d-flex flex-column flex-wrap border text-white rounded border-white p-3 bg-dark bg-opacity-25"
                 key={reservation.id}
               >
-                <h2>{reservation.car}</h2>
+                <h2 className="text-decoration-underline">{reservation.car}</h2>
                 <p>City: {reservation.city}</p>
                 <p>Pick-up Date: {reservation.pick_up}</p>
                 <p>Return Date: {reservation.return_date}</p>

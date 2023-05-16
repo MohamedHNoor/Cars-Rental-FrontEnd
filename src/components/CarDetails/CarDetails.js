4645f602f2f8077c02722d34a81d396352dbb53d
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-05-16 14:06:53 +0200

    Fix styles error

--- src/components/CarDetails/CarDetails.js
+++ src/components/CarDetails/CarDetails.js
@@ -1,13 +1,13 @@
-import React, { useEffect } from "react";
-import { useParams, Link } from "react-router-dom";
-import { IoIosArrowForward } from "react-icons/io";
-import { FiSettings } from "react-icons/fi";
-import { BsArrowRightCircle } from "react-icons/bs";
-import { BiLeftArrow } from "react-icons/bi";
-import "./CarDetails.css";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchCar } from "../../redux/slices/carSlice";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import { IoIosArrowForward } from 'react-icons/io';
+import { FiSettings } from 'react-icons/fi';
+import { BsArrowRightCircle } from 'react-icons/bs';
+import { BiLeftArrow } from 'react-icons/bi';
+import './CarDetails.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchCar } from '../../redux/slices/carSlice';
+import { useNavigate } from 'react-router-dom';
 function CarDetails(props) {
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -28,36 +28,36 @@ function CarDetails(props) {
           <img
             src={singleCar.photo}
             alt={singleCar.name}
-            style={{ width: "100%" }}
+            style={{ width: '100%' }}
           />
         </div>
       </div>
 
       <div className="d-flex flex-column justify-content-start align-items-center gap-4">
-        <h2>
-          {" "}
+        <h2 className="d-flex align-items-center gap-2">
+          {' '}
           <Link to="/">
             <button className="back-btn">
-              <BiLeftArrow fill="white" />
+              <BiLeftArrow fill="#97bf10" />
             </button>
           </Link>
           {singleCar.name}
         </h2>
         <ul>
-          <li className="d-flex">
-            <div>Finance fee</div>
-            <div>{singleCar.price} $</div>
+          <li className="d-flex gap-2">
+            <div>Finance fee:</div>
+            <div>${singleCar.price}</div>
           </li>
-          <li className="d-flex">
-            <div>Option to purchase fee</div>
-            <div>249$</div>
+          <li className="d-flex gap-2">
+            <div>Option to purchase fee:</div>
+            <div>$249</div>
           </li>
-          <li className="d-flex">
-            <div>Total amount payable</div>
-            <div>249$</div>
+          <li className="d-flex gap-2">
+            <div>Total amount payable:</div>
+            <div>$249</div>
           </li>
-          <li className="d-flex">
-            <div>Duration</div>
+          <li className="d-flex gap-2">
+            <div>Duration:</div>
             <div>48 months</div>
           </li>
         </ul>
@@ -65,11 +65,18 @@ function CarDetails(props) {
           <span>5.9% APR </span>
           Representative
         </div>
-        <Link to="/">
+        <Link
+          to="/"
+          className="d-flex justify-content-between align-items-center gap-2 nav-link"
+        >
           DISCOVER MORE MODELS
           <IoIosArrowForward fill="black" />
         </Link>
-        <button type="button" onClick={handleReserve}>
+        <button
+          type="button"
+          onClick={handleReserve}
+          className="arrow-reserve-btn d-flex justify-content-between align-items-center gap-2"
+        >
           <FiSettings /> Reserve <BsArrowRightCircle />
         </button>
       </div>

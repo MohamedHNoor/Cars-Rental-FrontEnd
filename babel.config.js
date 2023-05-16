65b98dcf9ae664673cacda583e43a557cfc46166
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-05-16 15:58:43 +0200

    Fix linters errors

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,17 @@
-module.exports = api => {
-	api.cache(true);
+module.exports = (api) => {
+  api.cache(true);
 
-	return {
-		presets: ["@babel/preset-env", "@babel/preset-react"],
-		plugins: [
-			"@babel/plugin-transform-runtime",
-			["@babel/plugin-proposal-class-properties", { loose: true }],
-		],
-		env: {
-			test: {
-				"plugins": ["@babel/plugin-transform-modules-commonjs"],
-				presets: [
-					["@babel/preset-env", { targets: { node: "current" } }],
-				],
-			},
-		},
-	};
+  return {
+    presets: ['@babel/preset-env', '@babel/preset-react'],
+    plugins: [
+      '@babel/plugin-transform-runtime',
+      ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ],
+    env: {
+      test: {
+        plugins: ['@babel/plugin-transform-modules-commonjs'],
+        presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
+      },
+    },
+  };
 };
